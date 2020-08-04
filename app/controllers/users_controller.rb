a3772cad3a0cea03570f16978396f61738038c9f
Noah Gluck <noahgluck@gmail.com>
2020-08-03 20:57:59 -0700

    finished up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,36 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.json
-  def show
-  end
+
 
   # GET /users/new
   def new
+    @user = User.new
   end
 
   # GET /users/1/edit
-
+def edit
+  @user = User.find(params[:id])
+end
   # POST /users
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
   # POST /users.json
   def create
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
   private
